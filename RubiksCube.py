224b45038b0c5eb83cb6391ede4038ebe8d4e0c7
nestoram29 <nam231243@gmail.com>
2026-03-29 17:15:58 -0700

    Print out some of cube to CLI

--- RubiksCube.py
+++ RubiksCube.py
@@ -1,21 +1,52 @@
+import numpy as np
+from termcolor import colored
+
 class RubiksCube:
+    bgColors = ('green', 'red', 'blue', 'magenta', 'white', 'yellow')
+    fgColors = ('white', 'white', 'white', 'white', 'black', 'black')
+
     def __init__(self, startState = None):
-        self.cube = [
-            *range(0, 9), # Up
-            *range(9, 18), # Down
-            *range(18, 27), # Left
-            *range(27, 36), # Right
-            *range(36, 45), # Front
-            *range(45, 54), # Back
-        ] # Centers = [4, 13, 22, 31, 40, 49]
+        self.cube = np.arange(3 * 3 * 6)
+    
+    def __str__(self):
+        def getCliStr(i):
+            return colored(
+                f'{i:2d}',
+                f'{RubiksCube.fgColors[int(i / 9) % 6]}',
+                f'on_{RubiksCube.bgColors[int(i / 9) % 6]}'
+                )
+        
+        s = ''
+        for i in range(0, 9, 3):
+            for j in range(i, 28 + i, 9):
+                for k in range(3):
+                    s += f'{getCliStr(j + k)} '
+            
+            s += '\n'
+        
+        return s
+
+#         return f'''
+#           {rowToStr(36, 39)}
+#           {rowToStr(39, 42)}
+#           {rowToStr(42, 45)}
+#          ------------
+# {rowToStr(0, 3)} | {rowToStr(9, 12)} | {rowToStr(18, 21)} | {rowToStr(27, 30)} 
+# {rowToStr(3, 6)} | {rowToStr(12, 1)} | {rowToStr(c[21, 24])} | {rowToStr(30, 33)} 
+# {rowToStr(6, 9)} | {rowToStr(15, 1)} | {rowToStr(24, 27)} | {rowToStr(33, 36)}
+#          ------------
+#           {rowToStr(45, 48)}
+#           {rowToStr(48, 51)}
+#           {rowToStr(51, )}
+# '''
 
     def scramble(self):
         pass
     
     def checkInValidState(self):
         pass
-
-    def makeMove(move):
+    
+    def makeMove(self, move):
         '''
         Notation
 
