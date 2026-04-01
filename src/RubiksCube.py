6726279ae617c3ca0bc5fc39598f9dffb0044005
nestoram29 <nam231243@gmail.com>
2026-03-31 21:47:52 -0700

    Finish moves implementation

--- src/RubiksCube.py
+++ src/RubiksCube.py
@@ -5,6 +5,40 @@ from termcolor import colored
 class RubiksCube:
     bgColors = ('green', 'red', 'blue', 'magenta', 'white', 'yellow')
 
+    faces = {
+        'L': slice(0, 9),
+        'F': slice(9, 18),
+        'R': slice(18, 27),
+        'B': slice(45, 54),
+        'U': slice(36, 45),
+        'D': slice(45, 54),
+    }
+
+    faceEdges = {
+        'L': [36, 39, 42, 9, 12, 15, 45, 48, 48, 51, 35, 32, 29],
+        'F': [42, 43, 44, 18, 21, 24, 47, 46, 45, 8, 5, 2],
+        'R': [44, 41, 38, 27, 30, 33, 53, 50, 47, 17, 14, 11],
+        'B': [38, 37, 36, 0, 3, 6, 51, 52, 53, 26, 23, 20],
+        'U': [29, 28, 27, 20, 19, 18, 11, 10, 9, 2, 1, 0],
+        'D': [15, 16, 17, 24, 25, 26, 33, 34, 35, 6, 7, 8],
+    }
+
+    slices = {
+        'M': [37, 40, 43, 10, 13, 16, 46, 49, 52, 34, 31, 28],
+        'E': [3, 4, 5, 12, 13, 14, 21, 22, 23, 30, 31, 32],
+        'S': [39, 40, 41, 19, 22, 25, 50, 49, 48, 7, 4, 1],
+    }
+
+    wideMoves = {
+        'u': 'E',
+        'd': 'E',
+        'r': 'M',
+        'l': 'M',
+        'f': 'S',
+        'b': 'S',
+    }
+
+
     def __init__(self, startState = None):
         self.cube = np.arange(3 * 3 * 6)
     
@@ -52,18 +86,42 @@ class RubiksCube:
         pass
     
     def makeMove(self, move):
-        '''
-        Notation
+        move = [c for c in move]
 
+        direction = -1 # Clockwise
+
+        if len(move) == 2:
+            direction = 2 if move[1] == '2' else 1            
+        
+        move = move[0]
+        if move in RubiksCube.faces:
+            self._faceMove(move, direction)
+        elif move in RubiksCube.slices:
+            self._sliceMove(move, direction)
+        else:
+            self._wideMove(move, direction)
+
+
+    def _faceMove(self, move, direction):
+        '''
         Face Turns:
         U  D  R  L  F  B  (clockwise)
         U' D' R' L' F' B' (counterclockwise)
         U2 D2 R2 L2 F2 B2
+        '''
+        face = RubiksCube.faces[move]
 
-        Wide Moves
-        u  d  r  l  f  b
-        u' d' r' l' f' b'
+        self.cube[face] = np.rot90(
+            self.cube[face].reshape(3, 3),
+            k = direction,
+        ).flatten()
 
+        edgeIndices = RubiksCube.faceEdges[move]
+
+        self.cube[edgeIndices] = np.roll(self.cube[edgeIndices], -3 * direction)
+
+    def _sliceMove(self, move, direction):
+        '''
         Slice Moves
         M  E  S
         M' E' S'
@@ -72,37 +130,21 @@ class RubiksCube:
         (E follows D)
         (S follows F)
         '''
-        move = [c for c in move]
+        sliceIndices = RubiksCube.slices[move]
+        self.cube[sliceIndices] = np.roll(self.cube[sliceIndices], -3 * direction)
 
-        face = move[0]
-        direction = -1 # Clockwise
-
-        if len(move) == 2:
-            direction = 2 if move[1] == '2' else 1            
-        
-        faceSlice = {
-            'L': slice(0, 9),
-            'F': slice(9, 18),
-            'R': slice(18, 27),
-            'B': slice(45, 54),
-            'U': slice(36, 45),
-            'D': slice(45, 54),
-        }
-        face = faceSlice[move[0]]
-
-        self.cube[face] = np.rot90(
-            self.cube[face].reshape(3, 3),
-            k = direction,
-        ).flatten()
-
-        # TODO: don't forget face edges
+    def _wideMove(self, move, direction):
+        '''
+        Wide Moves
+        u  d  r  l  f  b
+        u' d' r' l' f' b'
+        '''
+        self._faceMove(move.upper(), direction)
+        self._sliceMove(RubiksCube.wideMoves[move], direction)
 
 if __name__ == '__main__':
     r = RubiksCube()
     print(r)
 
-    r.makeMove('U')
-    print(r)
-
-    r.makeMove('L2')
+    r.makeMove('E+')
     print(r)
